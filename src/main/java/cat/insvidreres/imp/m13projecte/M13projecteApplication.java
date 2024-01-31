97a8c4fa4a3159918dc4d7ddb160f209a0802df8
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2024-01-31 09:20:42 +0100

    Initial commit

---
+++ src/main/java/cat/insvidreres/imp/m13projecte/M13projecteApplication.java
@@ -0,0 +1,13 @@
+package cat.insvidreres.imp.m13projecte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M13projecteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M13projecteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/insvidreres/imp/m13projecte/M13projecteApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.insvidreres.imp.m13projecte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M13projecteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
